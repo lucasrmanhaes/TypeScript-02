b6de0237f43c4a4d4b48ae7bd43fd44538a47f91
lucas <ramosmanhaes@outlook.com>
2024-07-24 09:46:47 -0300

    Criando estrutura do projeto

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,6 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
-import { Negociacoes } from './models/negociacoes.js';
 
 const negociacaoController = new NegociacaoController();
-
 const form = document.querySelector(".form");
 form.addEventListener('submit', event => {
     event.preventDefault();
